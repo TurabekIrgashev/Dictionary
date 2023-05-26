531d16ef54abd8fbc8d84d3287892a07a9939354
TurabekIrgashev <turabekirgashev2018@gmail.com>
2023-05-26 21:23:31 +0500

    versiya 2

--- src/components/Definitions/Definitions.js
+++ src/components/Definitions/Definitions.js
@@ -17,7 +17,7 @@ const Definitions = ({ meanings, word, LightTheme, category }) => {
                 color: LightTheme ? "white" : "black",
               }}
             >
-              <div>
+              <div className="border">
                 <h3>Word: {mean.headword.text} Pos: {mean.headword.pos} </h3>
                 <h3>Pronunciation: {mean.headword.pronunciation.value}</h3>
               </div>
@@ -26,35 +26,62 @@ const Definitions = ({ meanings, word, LightTheme, category }) => {
                   mean.senses?.map((def) =>
                     <div>
                       {
-                        (def.definition !== "") ? (
-                          <h3>Definition: {def.definition}</h3>
-                        ) : (console.log())
+                        (def.definition) ? (
+                          <div className="border">
+                            <h3>Definition: {def.definition}</h3>
+                            <div>
+                              {
+                                (def.examples) ? (
+                                  def.examples?.map((examples) =>
+                                    <div>
+                                      <h3>Example: {examples.text}</h3>
+                                    </div>
+                                  )
+                                ) : (console.log("DefExamples"))
+                              }
+                            </div>
+                          </div>
+                        ) : (console.log("Definition"))
                       }
+
                       <div>
                         {
-                          def.compositional_phrases?.map((phrasesDef) =>
-                            <div>
-                              <div>
-                                <h3>Compositional Phrases</h3>
-                                <h3>Text: {phrasesDef.text}</h3>
-                                {
-                                  phrasesDef.definition !== "" ? (
-                                    <h3>Definition: {phrasesDef.definition}</h3>
-                                  ) : (console.log())
-                                }
+                          (def.composition_phrases !== "") ? (
+                            def.compositional_phrases?.map((phrasesDef) =>
+                              <div className="border">
+                                <div>
+                                  <h3>Compositional Phrases</h3>
+                                  <h3>Text: {phrasesDef.text}</h3>
+                                  {
+                                    phrasesDef.definition ? (
+                                      <h3>Definition: {phrasesDef.definition}</h3>
+                                    ) : (console.log("Null"))
+                                  }
+                                  {
+                                    phrasesDef.senses ? (
+                                      phrasesDef.senses?.map((sensesDef) =>
+                                        <h3>Definition: {sensesDef.definition}</h3>
+                                      )
+                                    ) : (console.log("sensesDef"))
+                                  }
+                                </div>
+                                <div>
+                                  {phrasesDef.examples?.map((examp) =>
+                                    <div>
+                                      <h3>Example: {examp.text}</h3>
+                                      {/* {
+                                      Object.keys(examp.translations)?.map((translation) =>
+                                        <div>
+                                          <h3>{translation.br}</h3>
+                                        </div>
+                                      )
+                                    } */}
+                                    </div>
+                                  )}
+                                </div>
                               </div>
-                              <div>
-                                {phrasesDef.examples?.map((examp) =>
-                                  <div>
-                                    <h3>Example: {examp.text}</h3>
-                                    {
-                                    }
-                                  </div>
-                                )}
-                              </div>
-                            </div>
-                          )
-
+                            )
+                          ) : (console.log("Composition"))
                         }
                       </div>
                     </div>
@@ -64,7 +91,6 @@ const Definitions = ({ meanings, word, LightTheme, category }) => {
             </div>
           ))
       }
-      )
     </div >
   );
 };
